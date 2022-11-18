2ee3035a73fc58d8bf90ee4a394d5ffb3f8b0562
ysk91 <ysk91.engineer@gmail.com>
2022-11-18 17:01:00 +0900

    説明文を追加、見た目を整えた

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,7 @@
 class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, {presence: true, length: {maximum: 50} }
   validates :url, presence: true
   validates :spreadsheet_id, presence: true
 end
