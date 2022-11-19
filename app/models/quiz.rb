36df7a7e0c82b7dff487e9fb68493ef5f9c47e70
ysk91 <ysk91.engineer@gmail.com>
2022-11-19 10:18:31 +0900

    Quizモデルのテスト完了

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,4 +4,11 @@ class Quiz < ApplicationRecord
   validates :title, {presence: true, length: {maximum: 50} }
   validates :url, presence: true
   validates :spreadsheet_id, presence: true
+  validate :check_url
+
+  def check_url
+    unless self.url.start_with?("https://docs.google.com/spreadsheets/")
+      errors.add(:base, "そのURLは使用できません")
+    end
+  end
 end
